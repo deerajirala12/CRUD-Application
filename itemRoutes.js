356a458b67c1787584f4383b6a0c53bc11c93036
deerajirala12 <deeraj.irala04@gmail.com>
2025-06-26 21:53:21 +0530

    Fix Item model import path

--- itemRoutes.js
+++ itemRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Item = require('../models/Item');
+const Item = require('./Item');
 
 // CREATE
 router.post('/', async (req, res) => {
